0ac4aac82e3480830039747b0829e15b9b7e4492
Brian Cameron <bcameron1489@gmail.com>
2022-03-01 21:39:09 -0500

    Add error handling for user Auth on registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
@@ -11,7 +12,19 @@ def register(request):
         password2 = request.POST['password2']
 
         if password == password2:
-            return
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email,
+                    first_name=first_name, last_name=last_name)
+                    user.save()
+                    messages.success(request, 'You are now registered!!!')
+                    return redirect('login')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('register')
